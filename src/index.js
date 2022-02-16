182823a2ed0990e561e1bce2c5ab9c6683dc2e46
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-16 16:51:22 +0200

    installing react dnd and setting it up

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './assets/css/tailwind.css';
 import './assets/css/base.css';
 import App from './pages/App';
 import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 ReactDOM.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <App />
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
     </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
