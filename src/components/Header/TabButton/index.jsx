8fa3d0204951d9253c0e64533fa3e65174340125
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-16 12:10:25 +0200

    rotate tab arrow when it is clicked

--- src/components/Header/TabButton/index.jsx
+++ src/components/Header/TabButton/index.jsx
@@ -2,14 +2,19 @@ import { Button } from '@mui/material'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import classNames from 'classnames'
 import { useStyles } from './styles'
+import { useCallback, useState } from 'react'
 
 const TabButton = ({ label }) => {
     const classes = useStyles();
 
+    const [ open, setOpen ] = useState(false);
+    const toggle = useCallback(() => setOpen(o => !o), []);
+
     return (
-        <Button 
+        <Button
             className={classNames(classes.defaultButton, `border-0 rounded-none py-2.5`)}
-            endIcon={<KeyboardArrowDownIcon />}
+            endIcon={<KeyboardArrowDownIcon className={classNames({'rotate-180': open})} />}
+            onClick={toggle}
             variant="contained">
             { label }
         </Button>
