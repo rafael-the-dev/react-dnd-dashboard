a11368c419f939298edc1eb1300a27d9d29f0869
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-16 11:48:53 +0200

    adding menu and state toggler

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,19 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import classNames from 'classnames'
 import { useGlobalStyles } from '../../styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useCallback, useState } from 'react'
+import CloseIcon from '@mui/icons-material/Close';
 
 const Header = () => {
     const globalStyles = useGlobalStyles();
 
+    const [ open, setOpen ] = useState(false);
+
+    const toggleOpen = useCallback(() => setOpen(o => !o), [])
+
     return (
-        <header className={classNames(globalStyles.px, `py-4`)}>
+        <header className={classNames(globalStyles.px, `py-4 flex justify-between items-center`)}>
             <Typography 
                 className={classNames('font-bold')}
                 component="h1"
@@ -14,6 +21,11 @@ const Header = () => {
             >
                 Dashboard
             </Typography>
+            <div>
+                <IconButton onClick={toggleOpen}>
+                    { open ? <CloseIcon />  : <MenuIcon /> }
+                </IconButton>
+            </div>
         </header>
     );
 };
