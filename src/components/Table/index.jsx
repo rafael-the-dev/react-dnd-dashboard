299a5d6c4b9aa6a93df9a6347f34e28af832a1b4
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-16 16:23:17 +0200

    adding sales data

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,30 @@
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const DefaultTable = () => {
+    return (
+        <TableContainer 
+            component={Paper}
+            elevation={0}>
+            <Table aria-label="table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default DefaultTable;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,15 @@
 import classNames from 'classnames'
 import { useStyles } from './styles'
+import Table from '../../components/Table'
+import { useGlobalStyles } from '../../styles';
 
 const Home = () => {
     const classes = useStyles();
+    const globalStyles = useGlobalStyles();
 
     return (
-        <main>
-            
+        <main className={classNames(globalStyles.px, `py-12`)}>
+            <Table />
         </main>
     );
 };
