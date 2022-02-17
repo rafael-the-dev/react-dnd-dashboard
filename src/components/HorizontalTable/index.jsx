1dae9ff577d4bdc2635a75c66faa2a78fd01159c
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-17 08:43:32 +0200

    adding new feature - horizontal table

---
+++ src/components/HorizontalTable/index.jsx
@@ -0,0 +1,152 @@
+import { useDrop } from 'react-dnd'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { ItemTypes } from '../../config'
+import { useMemo, useRef, useState } from 'react'
+import data from '../../sales.json'
+import classNames from 'classnames'
+import { TablePagination } from '@mui/material';
+
+const DefaultTable = () => {
+    const [ columnsList, setColumnsList ] = useState(['', '']);
+    const isFirstRender = useRef(true);
+    const [ rowsPerPage, setRowsPerPage ] = useState(5);
+    const [ page, setPage ] = useState(0);
+
+    const [, drop] = useDrop(
+        () => ({
+            accept: ItemTypes.SALE_COLUMN,
+            drop: (item) => {
+                console.log(item);
+                isFirstRender.current = false;
+                setColumnsList(list => {
+                    return list.includes(item.column) ? list : [ ...list.filter(item => Boolean(item)), item.column ];
+                });
+            },
+            collect: (monitor) => ({
+                isOver: !!monitor.isOver()
+            })
+        }),
+        []
+    );
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const columnsListMemo = useMemo(() => (
+        <TableRow>
+            {
+                columnsList.map((column, index) => (
+                    <TableCell 
+                        align="center" 
+                        className={classNames(`bg-blue-800 text-slate-50`)}>
+                        { column }
+                    </TableCell>
+                ))
+            }
+        </TableRow>
+    ), [ columnsList ]);
+
+    const emptyTable = useMemo(() => (
+        <>
+            <TableRow>
+                <TableCell 
+                    align="center"
+                    className={classNames(`bg-blue-800 text-slate-50`)}>
+                </TableCell>
+                <TableCell 
+                    align="center">
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell 
+                    align="center"
+                    className={classNames(`bg-blue-800 text-slate-50`)}>
+                </TableCell>
+                <TableCell 
+                    align="center">
+                </TableCell>
+            </TableRow>
+        </>
+    ), [])
+
+    const rowsList = useMemo(() => {
+        const list = data.slice(page * rowsPerPage, (page * rowsPerPage) + rowsPerPage);
+        return isFirstRender.current ? emptyTable : (
+            columnsList.map((column, columnIndex) => (
+                <TableRow key={columnIndex}>
+                    <TableCell 
+                        align="center"
+                        className={classNames(`bg-blue-800 text-slate-50`)}
+                        key={columnIndex}>
+                        { column }
+                    </TableCell>
+                    {
+                        list.map((row, index) => (
+                            <TableCell 
+                                align="center"
+                                key={`${columnIndex}${index}`}>
+                                { row[column] }
+                            </TableCell>
+                        ))
+                    }
+                </TableRow>
+            )
+        ))
+    }, [ columnsList, emptyTable, page, rowsPerPage ])
+
+    data.slice(page * rowsPerPage, (page * rowsPerPage) + rowsPerPage).map((row, index) => (
+        <TableRow key={index}>
+            {
+                columnsList.map((column, columnIndex) => (
+                    <TableCell 
+                        align="center"
+                        key={`${index}${columnIndex}`}>
+                        { row[column] }
+                    </TableCell>
+                ))
+            }
+        </TableRow>
+    ))
+
+    return (
+        <Paper 
+            className={classNames(`w-fit max-w-full mb-6 mr-6`)}
+            elevation={0}>
+            <TableContainer 
+                className={classNames(`overflow-auto`)}
+                >
+                <Table 
+                    aria-label="table"
+                    sx={{ minWidth: 50 }}
+                    ref={drop}>
+                    <TableBody>
+                        { rowsList }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[5, 7, 10, 15, 25, 30]}
+                component="div"
+                count={data.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
+    );
+};
+
+export default DefaultTable;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import { useStyles } from './styles'
 import Table from '../../components/Table'
+import HorizontalTable from '../../components/HorizontalTable'
 import { useGlobalStyles } from '../../styles';
 import { useDrop } from 'react-dnd'
 import { ItemTypes } from '../../config'
@@ -13,12 +14,15 @@ const Home = () => {
 
     const [, drop] = useDrop(
         () => ({
-            accept: ItemTypes.TABLE,
+            accept: [ ItemTypes.TABLE ],
             drop: (item) => {
                 setComponents(list => {
                     if(item.type === 'vertical-table') {
                         return [ ...list, <Table key={list.length + 1} />]
+                    } else if(item.type === 'horizontal-table'){
+                        return [ ...list, <HorizontalTable key={list.length + 1} />]
                     }
+
                     return list;
                 })
             },
@@ -31,7 +35,7 @@ const Home = () => {
 
     return (
         <main 
-            className={classNames(globalStyles.px, `py-12 flex flex-wrap`)}
+            className={classNames(globalStyles.px, `py-12 flex flex-wrap items-start`)}
             ref={drop}>
             {
                 components
