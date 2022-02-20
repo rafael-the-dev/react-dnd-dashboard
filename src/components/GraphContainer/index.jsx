f55d69c48fc96a8c9e04907276fc2146da97406c
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-20 20:27:26 +0200

    creating default graphs container

---
+++ src/components/GraphContainer/index.jsx
@@ -0,0 +1,14 @@
+import Container from '../Container';
+
+
+const GraphContainer = () => {
+    return (
+        <Container>
+            <div>
+
+            </div>
+        </Container>
+    );
+};
+
+export default GraphContainer;
