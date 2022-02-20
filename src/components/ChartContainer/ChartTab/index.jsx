32a9ce30ccc94643d043f5db149adae148533ed5
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-20 21:06:36 +0200

    adding chart filters

---
+++ src/components/ChartContainer/ChartTab/index.jsx
@@ -0,0 +1,21 @@
+import { Button } from '@mui/material'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import classNames from 'classnames'
+import { useStyles } from './styles'
+//import { useCallback, useContext, useState } from 'react'
+
+const ChartTab = ({ label, tabID, openedTab, tabClickHanlder }) => {
+    const classes = useStyles();
+
+    return (
+        <Button
+            className={classNames(classes.defaultButton, `border-0 rounded-none py-2.5 sm:shadow-none sm:w-auto`)}
+            endIcon={<KeyboardArrowDownIcon className={classNames({'rotate-180': openedTab === tabID })} />}
+            onClick={tabClickHanlder(tabID)}
+            variant="contained">
+            { label }
+        </Button>
+    );
+};
+
+export default ChartTab;
--- src/components/ChartContainer/index.jsx
+++ src/components/ChartContainer/index.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import AreaChartContainer from '../AreaChart';
 import BarChartContainer from '../BarChart';
 import Container from '../Container';
@@ -6,8 +6,12 @@ import LineChartContainer from '../LineChart';
 import { ResponsiveContainer } from 'recharts';
 import { useDrag } from 'react-dnd'
 import { ItemTypes } from '../../config'
+import classNames from 'classnames'
+import ChartTab from './ChartTab'
 
 const ChartContainer = ({ chartType, componentID }) => {
+    const [ open, setOpen ] = useState('');
+    const tabClickHanlder = useCallback(prop => () => setOpen(prop), []);
     const chartTypes = useMemo(() => ({
         area: {
             component: <AreaChartContainer />,
@@ -33,8 +37,25 @@ const ChartContainer = ({ chartType, componentID }) => {
 
     return (
         <Container>
-            <div>
-
+            <div className={classNames(`pl-3 py-4 w-full flex items-stretch justify-start`)}>
+                <ChartTab 
+                    label="X axe"
+                    tabID="x-axe"
+                    openedTab={open}
+                    tabClickHanlder={tabClickHanlder}
+                />
+                <ChartTab 
+                    label="Y axe"
+                    tabID="y-axe"
+                    openedTab={open}
+                    tabClickHanlder={tabClickHanlder}
+                />
+                <ChartTab 
+                    label="Agreggations"
+                    tabID="agreggations"
+                    openedTab={open}
+                    tabClickHanlder={tabClickHanlder}
+                />
             </div>
             <ResponsiveContainer ref={drag} width="100%" height="90%">
                 { chartTypes[chartType].component }
