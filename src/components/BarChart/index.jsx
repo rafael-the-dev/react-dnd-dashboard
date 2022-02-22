a956083ffcdaeedbe6c3721aa89159fa55a963d8
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-22 17:32:13 +0200

    adding details to bar chart

--- src/components/BarChart/index.jsx
+++ src/components/BarChart/index.jsx
@@ -1,14 +1,46 @@
 import { Paper } from '@mui/material';
-import React, { useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import { BarChart , Legend, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import data from  '../../sales.json'
+//import data from  '../../sales.json'
 import { useGlobalStyles } from '../../styles'
 import classNames from 'classnames'
 import { useDrag, useDrop } from 'react-dnd'
 import { ItemTypes } from '../../config'
+import nextId from "react-id-generator";
 
-const BarChartContainer = ({ componentID }) => {
+const BarChartContainer = ({ componentID, colors, data, xAxeList, yAxeList }) => {
     const globalStyles = useGlobalStyles();
+
+    const index = useRef(0);
+
+    const yAxe = useMemo(() => {
+        index.current = 0;
+        return (
+            yAxeList.map(yAxeItem => {
+                if(index.current >= colors.length) {
+                    index.current = 0;
+                }
+                const actualIndex = index.current;
+                index.current += 1;
+
+                return (
+                    <Bar 
+                        type="monotone" 
+                        dataKey={yAxeItem} 
+                        fill={colors[actualIndex]} 
+                        key={nextId('y-axe')}
+                        stroke={colors[actualIndex]} 
+                    />
+                );
+            })
+        );
+    }, [ colors, yAxeList ]);
+
+    const xAxe = useMemo(() => (
+        xAxeList.map(xAxeItem => (
+            <XAxis dataKey={xAxeItem} key={nextId('x-axe')} />
+        ))
+    ), [ xAxeList ]);
 
     return (
         <ResponsiveContainer width="100%" height="100%">
@@ -25,10 +57,10 @@ const BarChartContainer = ({ componentID }) => {
             >
             <CartesianGrid strokeDasharray="3 3" />
             <Legend />
-            <XAxis dataKey="id" />
-            <YAxis />
             <Tooltip />
-            <Bar type="monotone" dataKey="Preço Unitário" stroke="#8884d8" fill="#8884d8" />
+            <YAxis />
+            { xAxe }
+            { yAxe }
         </BarChart >
     </ResponsiveContainer>
     );
